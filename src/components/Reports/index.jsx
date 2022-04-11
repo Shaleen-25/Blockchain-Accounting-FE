cf51e1d9c95626e83b14442948ff177be61fba18
Shaleen Kachhara <Shaleen.Kachhara1@aexp.com>
2022-04-11 20:07:09 +0530

    replace urls for report downloads

--- src/components/Reports/index.jsx
+++ src/components/Reports/index.jsx
@@ -1,104 +1,63 @@
-import { useState } from "react";
-import { useJsonToCsv } from "react-json-csv";
-import Select from "react-dropdown-select";
 import Button from "@material-ui/core/Button";
 
-const fields = {
-    accountName: "Particulars",
-    amountdr: "Debit",
-    amountcr: "Credit",
-  },
-  style = {
-    padding: "25px",
-  };
-
 const Reports = () => {
-  const { saveAsCsv } = useJsonToCsv();
-  const [reportData, setReportData] = useState([]);
-  const date = new Date();
-  const filename = `Trial Balance ${date.getDate()}-${
-    date.getMonth() + 1
-  }-${date.getFullYear()}`;
-
-  const reportTypes = [
-    { label: "Trial Balance", value: "Trial Balance", id: "tb" },
-  ];
-
-  const getReportData = async (id) => {
-    let totalCr = 0,
-      totalDr = 0;
-    const res = await fetch(`https://mlsubba.herokuapp.com/api/report/${id}`);
-    const data = await res.json();
-    const reportData = data?.["Trial Balance"]?.map(
-      ({ accountName, amountDR, amountCR }) => {
-        let amountcr = 0,
-          amountdr = 0;
-        if (amountCR > amountDR) {
-          amountcr = Math.abs(amountCR - amountDR);
-        } else {
-          amountdr = Math.abs(amountDR - amountCR);
-        }
-        totalCr += amountcr;
-        totalDr += amountdr;
-        return {
-          accountName,
-          amountdr,
-          amountcr,
-        };
-      }
-    );
-
-    reportData.push({
-      accountName: "Total",
-      amountdr: totalDr,
-      amountcr: totalCr,
-    });
-
-    setReportData(reportData);
-  };
-
   return (
     <>
       <div
         style={{
           margin: 30,
           display: "flex",
+          flexDirection: "column",
           height: "60%",
+          width: "30%",
+          margin: "auto",
           justifyContent: "center",
           alignItems: "center",
         }}
       >
-          
-        <div className="dropdown">
-          <h4 style={{ textAlign: "left", lineHeight: 0 }}>
-            Choose Report Format
-          </h4>
-          <Select
-            options={reportTypes}
-            onChange={(values) => {
-              getReportData(values[0].id);
+        <h4 style={{ textAlign: "left", lineHeight: 0 }}>
+          Download Report in xlsx format
+        </h4>
+        <Button fullWidth variant="outlined" className="submit">
+          <a
+            href="https://mlsubba.herokuapp.com/api/report/download/tb.xlsx"
+            style={{
+              width: "100%",
+              color: "white",
+              textDecoration: "none",
             }}
-          />
-          <br />
-          <br />
-          <Button
-            fullWidth
-            variant="outlined"
-            className="submit"
-            disabled={!reportData.length}
-            onClick={() =>
-              saveAsCsv({ data: reportData || [], style, fields, filename })
-            }
           >
-            Download Report
-          </Button>
-        </div>
+            Trial Balance
+          </a>
+        </Button>
+        <br />
+        <Button fullWidth variant="outlined" className="submit">
+          <a
+            href="https://mlsubba.herokuapp.com/api/report/download/income.xlsx"
+            style={{
+              width: "100%",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Income Statement
+          </a>
+        </Button>
+        <br />
+        <Button fullWidth variant="outlined" className="submit">
+          <a
+            href="https://mlsubba.herokuapp.com/api/report/download/bs.xlsx"
+            style={{
+              width: "100%",
+              color: "white",
+              textDecoration: "none",
+            }}
+          >
+            Balance Sheet
+          </a>
+        </Button>
+        <br />
       </div>
-<h2>Welcome to reports Section</h2>
-	<h4> Click to download reports</h4>	
-    <a href="https://mlsubba.herokuapp.com/api/report/download/tb.xlsx" class="btn btn-primary">Trial Balance</a>
-	<a href="https://mlsubba.herokuapp.com/api/report/download/income.xlsx" class="btn btn-primary">Income Statement</a>
-	<a href="https://mlsubba.herokuapp.com/api/report/download/bs.xlsx" class="btn btn-primary">Balance Sheet</a>
     </>
   );
 };
